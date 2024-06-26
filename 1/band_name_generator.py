7835637bc1dd3a440b121a0d7f8ce0c9ce14c865
Svetlin <167436978+zensii@users.noreply.github.com>
2024-06-26 12:06:28 +0300

    first commit

---
+++ 1/band_name_generator.py
@@ -0,0 +1,6 @@
+print('Hello there!')
+print('What is your hometown\'s name?')
+name = input()
+print('What is your pet\'s name?')
+pet_name = input()
+print('Band name could be: ' + name + ' ' + pet_name)
